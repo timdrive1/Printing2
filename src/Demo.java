3436e3f397c11d26811fb5cafc4be31281a7b8ed
timdrive1 <timdrive2013@gmail.com>
2017-03-15 15:49:17 +0300

    Initial commit

---
+++ src/Demo.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Тим on 15.03.2017.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Printer printer1 = new Printer(false, "1");
+        printer1.print();
+        printer1.print("Курлы-курлы");
+
+    }
+}
---
+++ src/Printer.java
@@ -0,0 +1,35 @@
+/**
+ * Created by Тим on 15.03.2017.
+ */
+public class Printer {
+    boolean isOn;
+    String modelNumber;
+
+    @Override
+    public String toString() {
+        return "Printer{" +
+                "isOn=" + isOn +
+                ", modelNumber='" + modelNumber + '\'' +
+                '}';
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public String getModelNumber() {
+        return modelNumber;
+    }
+
+    public Printer(boolean isOn, String modelNumber) {
+
+        this.isOn = isOn;
+        this.modelNumber = modelNumber;
+    }
+    public void print(){
+        System.out.println(modelNumber+" "+isOn);
+    }
+    public void print(String string){
+        System.out.println(string);
+    }
+}
