f7aeffc5f15376a3d986ff00a2cde0c4d60edcd0
MaxTheDeveloper <utemov1997@mail.ru>
2017-03-29 01:12:01 +0400

    Создать класс Scanner implements IMachine. Добавить поле типа IMachine и реализовать включение-выключение аналогично принтеру. В методы turnOn(), turnOff() добавить вывод строки “scanner”. В демо-классе создать объект сканера, включить и выключить его.

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,5 +1,25 @@
 /**
  * Created by Maxim on 28.03.2017.
  */
-public class Scanner {
+public class Scanner implements IMachine {
+    IMachine iMachine;
+    @Override
+    public void turnOn() {
+        iMachine.turnOn();
+        System.out.println("scanner");
+    }
+
+    @Override
+    public void turnOff() {
+        iMachine.turnOff();
+        System.out.println("scanner");
+    }
+
+    @Override
+    public boolean checkIsOn() {
+        if(iMachine.checkIsOn()){
+            return true;
+        }
+        else return false;
+    }
 }
