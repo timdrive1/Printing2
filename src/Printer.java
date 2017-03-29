d082429a95ce084d6e5f1d2534505712b5af9a28
timdrive1 <timdrive2013@gmail.com>
2017-03-29 14:07:36 +0300

    generic1

--- src/Printer.java
+++ src/Printer.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
  * Created by Тим on 15.03.2017.
  */
 public class Printer implements IMachine {
-    //private boolean isOn;
+    //generics
     private String modelNumber;
     public IMachine iMachine;
     PaperTray paperTray = new PaperTray();
