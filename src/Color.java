a6b169707d0660a92661dd248216797077f5587b
MaxTheDeveloper <utemov1997@mail.ru>
2017-03-29 14:34:01 +0400

    132

--- src/Color.java
+++ src/Color.java
@@ -5,7 +5,7 @@ public enum Color {
     RED ("Red", 0xff0000) , GREEN("Green", 0x00ff00), BLUE("Blue", 0x0000ff);
     String colorasString;
     int code;
-
+//123
 
     Color(String colorasString, int code) {
         this.colorasString = colorasString;
